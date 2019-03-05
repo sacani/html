2576f2de3cb12dec1daeca216697bb5e3e49b501
sacani <antoniofernandezcruz@hotmail.com>
2019-03-05 17:37:59 +0100

    deleted in work

--- node/nodeschool/javascripting/for-loop.js
+++ node/nodeschool/javascripting/for-loop.js
@@ -1,8 +1,7 @@
-var total
 total = 0
-var limit
 limit = 10
-for( var i = 0; i < limit; i = i + 1){
+for( i = 0; i < limit; i = i + 1){
   total = total + i
+  console.log( total, i )
 }
 console.log( total )
--- node/nodeschool/learnyounode/make-it-modular.js
+++ node/nodeschool/learnyounode/make-it-modular.js
@@ -10,4 +10,5 @@ function callback( error, data ){
     return
   }
   console.log( data )
+  return
 }
